a26d5db5c89ed4f8e8390db2919d34a5c3280c66
guitarbien <guitarbien@gmail.com>
2018-02-24 18:12:43 +0800

    使用 Symfony Request Response 處理 HTTP 和輸出

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -7,3 +7,11 @@ define('ROOT_DIR', dirname(__DIR__));
 require ROOT_DIR . '/vendor/autoload.php';
 
 \Tracy\Debugger::enable();
+
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+$content = 'Hello ' . $request->get('name', 'default');
+
+$response = new Symfony\Component\HttpFoundation\Response($content);
+$response->prepare($request);
+$response->send();
