6f7f1c518d3ff4e29d205aa651ba4ad1904f8c7e
guitarbien <guitarbien@gmail.com>
2018-02-25 14:17:43 +0800

    Denpendencies.php 內宣告 TempalteDirectory construct 所需傳的參數

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -2,11 +2,14 @@
 
 declare(strict_types=1);
 
+use App\Framework\Rendering\TemplateDirectory;
 use App\Framework\Rendering\TemplateRenderer;
 use App\Framework\Rendering\TwigTemplateRendererFactory;
 use Auryn\Injector;
 
 $injector = new Injector();
+
+$injector->define(TemplateDirectory::class, [':rootDirectory' => ROOT_DIR]);
 
 $injector->delegate(
     TemplateRenderer::class,
