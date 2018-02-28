b175057cbccf1aee751a3d19fef984c1b04b477c
guitarbien <guitarbien@gmail.com>
2018-02-28 22:13:28 +0800

    使用 $injector->alias() 設定 SubmissionsQuery interface 預設要注入的實作 class
    再透過 share() 告訴 $injector 不要重複建立要注入到 SubmissionsQuery  的物件

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use App\Framework\Rendering\TemplateDirectory;
 use App\Framework\Rendering\TemplateRenderer;
 use App\Framework\Rendering\TwigTemplateRendererFactory;
+use App\FrontPage\Infrastructure\MockSubmissionQuery;
+use App\FrontPage\Presentation\SubmissionsQuery;
 use Auryn\Injector;
 
 $injector = new Injector();
@@ -18,5 +20,11 @@ $injector->delegate(
         return $factory->create();
     }
 );
+
+$injector->alias(SubmissionsQuery::class, MockSubmissionQuery::class);
+
+// Use share() prevent the injector creating a new instance whenever an object is injected
+// The same instance of the object is reused for all classes that use this dependency.
+$injector->share(SubmissionsQuery::class);
 
 return $injector;
