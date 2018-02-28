421ce7545b67e489e1a9755e97d2b6c21b482640
guitarbien <guitarbien@gmail.com>
2018-02-28 22:11:45 +0800

    目前還沒實作資料庫，所以用 Mock object 實作 SubmissionsQuery Interface，直接寫死回傳值

---
+++ src/FrontPage/Infrastructure/MockSubmissionQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FrontPage\Infrastructure;
+
+use App\FrontPage\Application\Submission;
+use App\FrontPage\Presentation\SubmissionsQuery;
+
+/**
+ * Class MockSubmissionQuery
+ * @package App\FrontPage\Infrastructure
+ */
+final class MockSubmissionQuery implements SubmissionsQuery
+{
+    /** @var Submission[] */
+    private $submissions;
+
+    /**
+     * MockSubmissionQuery constructor.
+     */
+    public function __construct()
+    {
+        $this->submissions = [
+            new Submission('https://duckduckgo.com', 'DuckDuckGo'),
+            new Submission('https://google.com', 'Google'),
+            new Submission('https://bing.com', 'Bing'),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function execute(): array
+    {
+        return $this->submissions;
+    }
+}
