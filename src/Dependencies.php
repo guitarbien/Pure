f819bedee76b9c6bacaf6cc2787c7f2b932f36b5
guitarbien <guitarbien@gmail.com>
2018-03-03 21:10:06 +0800

    設定 Database Connection 並使用 share() 避免重複建立連線

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -2,12 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Framework\Dbal\ConnectionFactory;
+use App\Framework\Dbal\DatabaseUrl;
 use App\Framework\Rendering\TemplateDirectory;
 use App\Framework\Rendering\TemplateRenderer;
 use App\Framework\Rendering\TwigTemplateRendererFactory;
 use App\FrontPage\Infrastructure\MockSubmissionQuery;
 use App\FrontPage\Presentation\SubmissionsQuery;
 use Auryn\Injector;
+use Doctrine\DBAL\Connection;
 
 $injector = new Injector();
 
@@ -29,5 +32,17 @@ $injector->alias(SubmissionsQuery::class, MockSubmissionQuery::class);
 // Use share() prevent the injector creating a new instance whenever an object is injected
 // The same instance of the object is reused for all classes that use this dependency.
 $injector->share(SubmissionsQuery::class);
+
+//-----------------------
+// Database Access Layer
+//-----------------------
+$injector->define(DatabaseUrl::class, [':url' => 'sqlite:///' . ROOT_DIR . '/storage/db.sqlite3']);
+
+$injector->delegate(Connection::class, function() use($injector): Connection {
+    $factory = $injector->make(ConnectionFactory::class);
+    return $factory->create();
+});
+
+$injector->share(Connection::class);
 
 return $injector;
