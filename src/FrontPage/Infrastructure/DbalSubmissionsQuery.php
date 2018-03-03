c45a8a9818ef60fa61df1bfeba812cb76ce5b454
guitarbien <guitarbien@gmail.com>
2018-03-03 21:58:06 +0800

    建立 DbalSubmissionsQuery 實際使用 DBAL 查詢 Submissions 的內容

---
+++ src/FrontPage/Infrastructure/DbalSubmissionsQuery.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FrontPage\Infrastructure;
+
+use App\FrontPage\Application\Submission;
+use App\FrontPage\Presentation\SubmissionsQuery;
+use Doctrine\DBAL\Connection;
+
+/**
+ * Class DbalSubmissionsQuery
+ * @package App\FrontPage\Infrastructure
+ */
+final class DbalSubmissionsQuery implements SubmissionsQuery
+{
+    /** @var Connection */
+    private $connection;
+
+    /**
+     * DbalSubmissionsQuery constructor.
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @return array
+     */
+    public function execute(): array
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $queryBuilder->addSelect('title');
+        $queryBuilder->addSelect('url');
+        $queryBuilder->from('submissions');
+        $queryBuilder->orderBy('creation_date', 'DESC');
+
+        $stmt = $queryBuilder->execute();
+        $rows = $stmt->fetchAll();
+
+        /** @var Submission[] $submissions */
+        $submissions = [];
+        foreach ($rows as $row) {
+            $submissions[] = new Submission($row['url'], $row['title']);
+        }
+
+        return $submissions;
+    }
+}
