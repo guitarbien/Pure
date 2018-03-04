c6ab4f113eacf573c4e4d92d9ffcf7bd1950d952
guitarbien <guitarbien@gmail.com>
2018-03-04 14:21:00 +0800

    建立 Token VO 並直接使用靜態 generate() method 產生 Token 物件
    PHP 7 加入了 random_bytes() 和 andrandom_int() 可以產生安全的亂數

---
+++ src/Framework/Csrf/Token.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Framework\Csrf;
+
+/**
+ * Class Token
+ * @package App\Framework\Csrf
+ */
+final class Token
+{
+    /** @var string */
+    private $token;
+
+    /**
+     * Token constructor.
+     * @param string $token
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function toString(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @return Token
+     * @throws \Exception
+     */
+    public static function generate(): self
+    {
+        $token = bin2hex(random_bytes(256));
+
+        return new self($token);
+    }
+}
