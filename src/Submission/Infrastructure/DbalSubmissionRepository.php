e5deb54567b96dbdc6dc1a5ba57d01b41dfd1b94
guitarbien <guitarbien@gmail.com>
2018-03-06 23:02:32 +0800

    prevent SQL injection with DBAL named parameters

--- src/Submission/Infrastructure/DbalSubmissionRepository.php
+++ src/Submission/Infrastructure/DbalSubmissionRepository.php
@@ -31,14 +31,16 @@ final class DbalSubmissionRepository implements SubmissionRepository
      */
     public function add(Submission $submission): void
     {
-        $this->connection->exec("
-            INSERT INTO
-            submissions (id, title, url, creation_date) VALUES (
-                '{$submission->getId()->toString()}', 
-                '{$submission->getTitle()}',
-                '{$submission->getUrl()}', 
-                '{$submission->getCreationDate()->format('Y-m-d H:i:s')}'
-            );
-        ");
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $queryBuilder->insert('submissions');
+        $queryBuilder->values([
+            'id'            => $queryBuilder->createNamedParameter($submission->getId()->toString()),
+            'title'         => $queryBuilder->createNamedParameter($submission->getTitle()),
+            'url'           => $queryBuilder->createNamedParameter($submission->getUrl()),
+            'creation_date' => $queryBuilder->createNamedParameter($submission->getCreationDate(), 'datetime'),
+        ]);
+
+        $queryBuilder->execute();
     }
 }
