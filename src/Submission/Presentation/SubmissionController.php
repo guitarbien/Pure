6145b0ac3dbfefeb661cb396060d4d700edae8fe
guitarbien <guitarbien@gmail.com>
2018-03-06 21:44:26 +0800

    SubmissionController 注入 SubmitLinkHandler，實作寫入資料

--- src/Submission/Presentation/SubmissionController.php
+++ src/Submission/Presentation/SubmissionController.php
@@ -8,6 +8,8 @@ use App\Framework\Csrf\StoredTokenValidator;
 use App\Framework\Csrf\Token;
 use App\Framework\MessageContainer\FlashMessenger;
 use App\Framework\Rendering\TemplateRenderer;
+use App\Submission\Application\SubmitLink;
+use App\Submission\Application\SubmitLinkHandler;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,20 +29,26 @@ final class SubmissionController
     /** @var FlashMessenger */
     private $flashMessenger;
 
+    /** @var SubmitLinkHandler */
+    private $submitLinkHandler;
+
     /**
      * SubmissionController constructor.
      * @param TemplateRenderer $templateRenderer
      * @param StoredTokenValidator $storedTokenValidator
      * @param FlashMessenger $flashMessenger
+     * @param SubmitLinkHandler $submitLinkHandler
      */
     public function __construct(
         TemplateRenderer $templateRenderer,
         StoredTokenValidator $storedTokenValidator,
-        FlashMessenger $flashMessenger
+        FlashMessenger $flashMessenger,
+        SubmitLinkHandler $submitLinkHandler
     ) {
         $this->templateRenderer     = $templateRenderer;
         $this->storedTokenValidator = $storedTokenValidator;
         $this->flashMessenger       = $flashMessenger;
+        $this->submitLinkHandler    = $submitLinkHandler;
     }
 
     /**
@@ -67,7 +75,10 @@ final class SubmissionController
             return $response;
         }
 
-        // save the submission
+        $this->submitLinkHandler->handle(new SubmitLink(
+            $request->get('url'),
+            $request->get('title')
+        ));
 
         $this->flashMessenger->add('success', 'Your URL was added successfully');
 
