0ed530f62170142898b00a2438ad3e8e57d3beab
guitarbien <guitarbien@gmail.com>
2018-03-21 23:44:28 +0800

    建立 handler 執行註冊行為

---
+++ src/User/Application/RegisterUserHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Application;
+
+use App\User\Domain\User;
+use App\User\Domain\UserRepository;
+
+/**
+ * Class RegisterUserHandler
+ * @package App\User\Application
+ */
+final class RegisterUserHandler
+{
+    /** @var UserRepository */
+    private $userRepository;
+
+    /**
+     * RegisterUserHandler constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param RegisterUser $command
+     */
+    public function handle(RegisterUser $command): void
+    {
+        $user = User::register($command->getEmail(), $command->getPassword());
+        $this->userRepository->add($user);
+    }
+}
