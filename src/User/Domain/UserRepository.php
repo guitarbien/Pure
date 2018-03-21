b185e60653bc2d730f33ca460a4e1ee65c5fd418
guitarbien <guitarbien@gmail.com>
2018-03-21 23:35:46 +0800

    建立 UserRepository interface

---
+++ src/User/Domain/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Domain;
+
+/**
+ * Interface UserRepository
+ * @package App\User\Domain
+ */
+interface UserRepository
+{
+    /**
+     * @param User $user
+     */
+    public function add(User $user): void;
+}
