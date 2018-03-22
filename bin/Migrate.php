2ca3323c4e2f9baa2f6686b43974c3fc6b669341
guitarbien <guitarbien@gmail.com>
2018-03-23 00:05:48 +0800

    重寫 Migrate 在 CLI 提供互動介面選擇要執行的 migration 檔案

--- bin/Migrate.php
+++ bin/Migrate.php
@@ -2,19 +2,62 @@
 
 declare(strict_types=1);
 
-use Auryn\Injector;
-use Doctrine\DBAL\Connection;
-use Migrations\Migration201803032130;
-
 define('ROOT_DIR', dirname(__DIR__));
 
 require ROOT_DIR . '/vendor/autoload.php';
 
-/** @var Injector $injector */
 $injector   = include(ROOT_DIR . '/src/Dependencies.php');
-$connection = $injector->make(Connection::class);
+$connection = $injector->make('Doctrine\DBAL\Connection');
 
-$migration = new Migration201803032130($connection);
-$migration->migrate();
+$migrations = get_available_migrations();
+$selected   = select_migration($migrations);
+
+foreach ($migrations as $key => $migration) {
+    if ($selected !== 0 && $selected !== $key + 1) {
+        continue;
+    }
+
+    $class = "Migrations\\$migration";
+    (new $class($connection))->migrate();
+
+    echo "Running $migration..." . PHP_EOL;
+}
 
 echo 'Finished running migrations' . PHP_EOL;
+
+/**
+ * @return array
+ */
+function get_available_migrations(): array
+{
+    $migrations = [];
+
+    foreach (new FilesystemIterator(ROOT_DIR . '/migrations') as $file) {
+        $migrations[] = $file->getBasename('.php');
+    }
+
+    return array_reverse($migrations);
+}
+
+/**
+ * @param array $migrations
+ * @return int
+ */
+function select_migration(array $migrations): int
+{
+    echo "[0] All" . PHP_EOL;
+
+    foreach ($migrations as $key => $name) {
+        $index = $key + 1;
+        echo "[$index] $name" . PHP_EOL;
+    }
+
+    $selected    = readline('Select the migration that you want tot run: ');
+    $selectedKey = $selected - 1;
+
+    if ($selected !== '0' && !array_key_exists($selectedKey, $migrations)) {
+        exit('Invalid selection' . PHP_EOL);
+    }
+
+    return (int)$selected;
+}
