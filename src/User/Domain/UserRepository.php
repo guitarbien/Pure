122518549bcce377332209d1bfa136c8b9f6efd6
guitarbien <guitarbien@gmail.com>
2018-03-25 20:42:51 +0800

    UserRepository interface 建立 save 和 findByEmail method

--- src/User/Domain/UserRepository.php
+++ src/User/Domain/UserRepository.php
@@ -14,4 +14,15 @@ interface UserRepository
      * @param User $user
      */
     public function add(User $user): void;
+
+    /**
+     * @param User $user
+     */
+    public function save(User $user): void;
+
+    /**
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User;
 }
