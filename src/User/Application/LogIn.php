bf0c8b35f7bf2a67b650bf3bb550e7e2d11b8d2e
guitarbien <guitarbien@gmail.com>
2018-03-25 20:28:05 +0800

    建立 LogIn command (value object)

---
+++ src/User/Application/LogIn.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Application;
+
+/**
+ * Class LogIn
+ * @package App\User\Application
+ */
+final class LogIn
+{
+    /** @var string */
+    private $email;
+
+    /** @var string */
+    private $password;
+
+    /**
+     * LogIn constructor.
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct(string $email, string $password)
+    {
+        $this->email    = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
