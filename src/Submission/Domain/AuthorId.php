b91f2e36ed3957ed056ef24eae2b4265447b3600
guitarbien <guitarbien@gmail.com>
2018-04-15 23:55:51 +0800

    建立 Value Object AuthorId

---
+++ src/Submission/Domain/AuthorId.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Submission\Domain;
+
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * Class AuthorId
+ * @package App\Submission\Domain
+ */
+final class AuthorId
+{
+    /** @var string */
+    private $id;
+
+    /**
+     * AuthorId constructor.
+     * @param string $id
+     */
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param UuidInterface $uuid
+     * @return AuthorId
+     */
+    public static function fromUuid(UuidInterface $uuid): AuthorId
+    {
+        return new AuthorId($uuid->toString());
+    }
+
+    /**
+     * @return string
+     */
+    public function toString(): string
+    {
+        return $this->id;
+    }
+}
