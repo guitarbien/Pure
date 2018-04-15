a964a022a3e5ef94e3a3c04f27e6f83c8144a84a
guitarbien <guitarbien@gmail.com>
2018-04-16 00:09:51 +0800

    首頁呈現多 join users 取得 author email

--- src/FrontPage/Infrastructure/DbalSubmissionsQuery.php
+++ src/FrontPage/Infrastructure/DbalSubmissionsQuery.php
@@ -33,10 +33,12 @@ final class DbalSubmissionsQuery implements SubmissionsQuery
     {
         $queryBuilder = $this->connection->createQueryBuilder();
 
-        $queryBuilder->addSelect('title');
-        $queryBuilder->addSelect('url');
+        $queryBuilder->addSelect('submissions.title');
+        $queryBuilder->addSelect('submissions.url');
+        $queryBuilder->addSelect('authors.email');
         $queryBuilder->from('submissions');
-        $queryBuilder->orderBy('creation_date', 'DESC');
+        $queryBuilder->join('submissions', 'users', 'authors', 'submissions.author_user_id = authors.id');
+        $queryBuilder->orderBy('submissions.creation_date', 'DESC');
 
         $stmt = $queryBuilder->execute();
         $rows = $stmt->fetchAll();
@@ -44,7 +46,7 @@ final class DbalSubmissionsQuery implements SubmissionsQuery
         /** @var Submission[] $submissions */
         $submissions = [];
         foreach ($rows as $row) {
-            $submissions[] = new Submission($row['url'], $row['title']);
+            $submissions[] = new Submission($row['url'], $row['title'],  $row['email']);
         }
 
         return $submissions;
