a1563d03544eac6674cba1e7c0be815039b9c752
guitarbien <guitarbien@gmail.com>
2018-05-13 20:39:30 +0800

    close the constructor

--- src/Submission/Domain/AuthorId.php
+++ src/Submission/Domain/AuthorId.php
@@ -19,7 +19,7 @@ final class AuthorId
      * AuthorId constructor.
      * @param string $id
      */
-    public function __construct(string $id)
+    private function __construct(string $id)
     {
         $this->id = $id;
     }
